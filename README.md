60a3517702ab38e9f299ea1b309295812e3314af
Emanuele Correa <emanuele.correa@rhitmo.com.br>
2023-02-15 10:21:05 -0300

    Finalizando modulo de design system

---
+++ README.md
@@ -0,0 +1,9 @@
+- [x] Text
+- [x] Heading
+- [x] Box
+- [x] Button
+- [x] TextInput
+- [x] TextArea
+- [x] CheckBox
+- [x] Avatar
+- [x] MultiStep
---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,28 @@
+import {
+  colors,
+  fonts,
+  fontSizes,
+  fontWeights,
+  lineHeights,
+  radii,
+  space,
+} from '@ignite-ui/tokens'
+import { createStitches, defaultThemeMap } from '@stitches/react'
+
+export const { styled, css, globalCss, keyframes, theme, createTheme, config } =
+  createStitches({
+    themeMap: {
+      ...defaultThemeMap,
+      height: 'space',
+      with: 'space',
+    },
+    theme: {
+      colors,
+      fontSizes,
+      fontWeights,
+      fonts,
+      lineHeights,
+      radii,
+      space,
+    },
+  })
